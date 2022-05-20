48fa3f418a86c51648998f75587e5bbaf04e9d09
Minglu Gu <gmlsyjsh@gmail.com>
2022-05-20 15:04:59 -0300

    Java Operators

--- 20211012/src/TestDemo.java
+++ 20211012/src/TestDemo.java
@@ -6,7 +6,88 @@
  *              (w3schools) Wrapper classes provide a way to use primitive data types (int, boolean, etc..) as objects.
  */
 public class TestDemo {
+
     public static void main(String[] args) {
+        // & ^ ! ~
+        // ternary operator
+        int a = 10;
+        int b = 20;
+        int max = a > b ? a : b;
+        System.out.println(max);
+    }
+    public static void main14(String[] args) {
+        System.out.println(10 > 20 && 10 / 0 == 0); // false
+        // System.out.println(10 > 20 || 10 / 0 == 0); // err exception / by zero
+        // System.out.println(10 < 20 || 10 / 0 == 0); // err exception / by zero
+    }
+
+    public static void main13(String[] args) {
+        // logical operators: && || !
+        // && support shortcut: condition 1 && condition 2, if condition 1 is false, it will skip condition 2
+        // ||: if 1 condition is true, return true. Support shortcut too as && And operator.
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        System.out.println(a < b && b < c); // t
+        System.out.println(a < b || b > c); // t
+        System.out.println(!(a < b)); // f
+
+
+    }
+    public static void main12(String[] args) {
+        //relational operators: == != < > <= >=, return true or false
+        int a = 10;
+        int b = 20;
+        System.out.println(a == b); // f
+        System.out.println(a != b); // t
+        System.out.println(a < b); // t
+        System.out.println(a > b); // f
+        System.out.println(a <= b); // t
+        System.out.println(a >= b); // f
+    }
+
+    public static void main11(String[] args) {
+        int a = 10;
+        a++;
+        System.out.println(a); // 11
+
+        int c = 10;
+        ++c;
+        System.out.println(c); // 11
+
+        int a1 = a++; // a1 = a, a = a + 1; a1 != c2
+        int c2 = ++c; // c = c + 1, c2 = c; a1 != c2
+        System.out.println(a1); //11
+        System.out.println(c2); //12
+    }
+
+    public static void main10(String[] args) {
+        int a = 10;
+        a += 1;
+        System.out.println(a);
+
+        short s = 10;
+        // s = s + 9; err
+        s = (short) (s + 9);
+        s += 9;
+    }
+
+    public static void main9(String[] args) {
+        // + - * / %
+        int a = 1;
+        int b = 2;
+        // a/b, divisor b can't be 0. / by zero exception
+        System.out.println(a/b); // 0
+
+        System.out.println(10 % 3); //1
+        System.out.println(11.5 % 2); //1.5
+        System.out.println(-10 % 3); // -1
+        System.out.println(10 % -3); // 1
+        System.out.println(-10 % -3); // -1
+    }
+
+    public static void main8(String[] args) {
         // wrapper class
         // int -> Integer char->Character
         String str = "123";
